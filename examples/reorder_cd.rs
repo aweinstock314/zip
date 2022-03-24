388a2f80faba36207bdb1e5b6a6dc5a9a0eb02ca
Avi Weinstock <aweinstock314@gmail.com>
2022-03-24 14:12:43 -0400

    Further changes to `reorder_zip` example (attempt to pack an orphaned LFH at the beginning, patching up the remainder of the offsets with no other changes).

--- examples/reorder_cd.rs
+++ examples/reorder_cd.rs
@@ -1,10 +1,14 @@
+use byteorder::{LittleEndian, WriteBytesExt};
 use std::io::{Read, Write, Cursor};
 use zip::{spec::{CentralDirectoryEnd, CentralDirectoryHeader, LocalFileHeader, GeneralPurposeBitFlags}};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let filename = "test.zip";
-    let path = std::path::Path::new(filename);
-    let mut file = std::fs::File::open(path)?;
+	let argv = ::std::env::args().collect::<Vec<_>>();
+	if argv.len() < 3 {
+		println!("Usage: reorder_cd input.zip output.zip");
+		return Ok(());
+	}
+    let mut file = std::fs::File::open(&argv[1])?;
     let mut bytes = Vec::new();
     file.read_to_end(&mut bytes)?;
     let mut cursor = Cursor::new(bytes.clone());
@@ -21,42 +25,67 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 		last_mod_time: 0,
 		last_mod_date: 0,
 		crc32: 0,
-		compressed_size: 0,
-		uncompressed_size: 0,
+		compressed_size: 8,
+		uncompressed_size: 8,
 		file_name_raw: b"mask".to_vec(),
 		extra_field: Vec::new(),
 	};
-	let padding = mask.len() + eocd.len();
-	let mut output = vec![0; mask.len() + bytes.len()];
-	let mut cdh_i = mask.len();
-	let mut lfh_i = padding + eocd.central_directory_size as usize;
+	let padding = mask.len() + 8;
+	//let mut output = vec![0; mask.len() + (1 << 20) + bytes.len()];
+	let mut cdh_i = 0;
+	let mut lfh_i = padding;
+	let mut cdhs = Vec::new();
+	let mut lfhs = Vec::new();
 
 	let mut cd_cursor = Cursor::new(cd);
 	while let Ok(mut cdh) = CentralDirectoryHeader::parse(&mut cd_cursor) {
 		let lfh_start = cdh.offset as usize;
-		let lfh_len = 30 + cdh.file_name_raw.len() + cdh.extra_field.len();
-		let lfh_end = lfh_start + lfh_len;
-		let data_end = lfh_end + cdh.compressed_size as usize;
-		let file_data = &bytes[lfh_start..lfh_end];
+		let file_data = &bytes[lfh_start..];
 		let mut file_cursor = Cursor::new(file_data);
 		let lfh = LocalFileHeader::parse(&mut file_cursor)?;
+		let lfh_end = lfh_start + lfh.len();
+		let data_end = lfh_end + cdh.compressed_size as usize;
 		let file_bytes = &bytes[lfh_end..data_end];
 		cdh.offset = lfh_i as u32;
-		cdh.write(&mut &mut output[cdh_i..cdh_i + cdh.len()])?;
+		//cdh.write(&mut &mut output[cdh_i..cdh_i + cdh.len()])?;
 		cdh_i += cdh.len();
-		lfh.write(&mut &mut output[lfh_i..lfh_i+lfh_len])?;
-		lfh_i += lfh_len;
+		cdhs.push(cdh);
+		//lfh.write(&mut &mut output[lfh_i..lfh_i+lfh.len()])?;
+		lfh_i += lfh.len();
+		//(&mut output[lfh_i..lfh_i+file_bytes.len()]).copy_from_slice(&file_bytes);
+		lfh_i += file_bytes.len();
+		lfhs.push((lfh, file_bytes.to_vec()));
+	}
+
+	let mut output = vec![0; cdh_i + lfh_i + eocd.len()];
+	let mut lfh_i = padding;
+	for (lfh, file_bytes) in lfhs.iter() {
+		println!("{:06x}: {:?}", lfh_i, lfh);
+		lfh.write(&mut &mut output[lfh_i..lfh_i+lfh.len()])?;
+		lfh_i += lfh.len();
 		(&mut output[lfh_i..lfh_i+file_bytes.len()]).copy_from_slice(&file_bytes);
 		lfh_i += file_bytes.len();
 	}
-	eocd.central_directory_offset = mask.len() as u32;
-	eocd.write(&mut &mut output[cdh_i..cdh_i+eocd.len()])?;
+	let mut cdh2_i = lfh_i;
+	for cdh in cdhs.iter() {
+		println!("{:06x}: {:?}", cdh2_i, cdh);
+		cdh.write(&mut &mut output[cdh2_i..cdh2_i + cdh.len()])?;
+		cdh2_i += cdh.len();
+	}
 
-	mask.compressed_size = (lfh_i - mask.len()) as u32;
-	mask.uncompressed_size = (lfh_i - mask.len()) as u32;
+	assert_eq!(eocd.central_directory_size as usize, cdh2_i - lfh_i);
+	//eocd.central_directory_offset = mask.len() as u32;
+	//eocd.write(&mut &mut output[cdh_i..cdh_i+eocd.len()])?;
+	eocd.central_directory_offset = lfh_i as u32;
+	eocd.write(&mut &mut output[cdh2_i..cdh2_i+eocd.len()])?;
+
+	//mask.compressed_size = (lfh_i - mask.len()) as u32;
+	//mask.uncompressed_size = (lfh_i - mask.len()) as u32;
 	mask.write(&mut &mut output[0..mask.len()])?;
+	(&mut &mut output[mask.len()..mask.len()+4]).write_u32::<LittleEndian>(lfh_i as u32)?;
+	(&mut &mut output[mask.len()+4..mask.len()+8]).write_u32::<LittleEndian>((cdh2_i - lfh_i) as u32)?;
 
-	if let Ok(mut outfile) = std::fs::File::create("test2.zip") {
+	if let Ok(mut outfile) = std::fs::File::create(&argv[2]) {
 		outfile.write_all(&output[..])?;
 	}
 
